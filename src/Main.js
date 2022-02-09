2df4cf48cb98607845330817b42b39423dd343fd
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-02-08 19:21:16 -0500

    add left, right, and random btn

--- src/Main.js
+++ src/Main.js
@@ -1,43 +1,60 @@
-import React, { useState } from 'react';
-import players from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandDots, faShield, faToolbox, faFutbol, faPersonRunning } from '@fortawesome/free-solid-svg-icons'
-
+import React, { useState } from "react";
+import players from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHandDots,
+  faShield,
+  faToolbox,
+  faFutbol,
+  faPersonRunning,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
-  const [index, setIndex] = useState(0);
-  const { name, position, nationality, squad_number, image, career} = players[index];
+  const [index, setIndex] = useState(8);
+  const { name, position, nationality, squad_number, image, career } =
+    players[index];
 
   const setPlayerIcon = (position) => {
-
     switch (position) {
-      case 'goalkeeper':
-        return <FontAwesomeIcon icon={faHandDots} />
-      case 'defender':
-        return <FontAwesomeIcon icon={faShield} />
-      case 'midfielder':
-        return <FontAwesomeIcon icon={faToolbox} />
-      case 'winger':
-        return <FontAwesomeIcon icon={faPersonRunning} />
-      case 'striker':
-        return <FontAwesomeIcon icon={faFutbol} />
+      case "goalkeeper":
+        return <FontAwesomeIcon icon={faHandDots} />;
+      case "defender":
+        return <FontAwesomeIcon icon={faShield} />;
+      case "midfielder":
+        return <FontAwesomeIcon icon={faToolbox} />;
+      case "winger":
+        return <FontAwesomeIcon icon={faPersonRunning} />;
+      case "striker":
+        return <FontAwesomeIcon icon={faFutbol} />;
       default:
-        return <FontAwesomeIcon icon={faFutbol} />
+        return <FontAwesomeIcon icon={faFutbol} />;
     }
-  }
-
+  };
 
   return (
-    <article className='review'>
+    <article className="review">
       <div className="img-container">
-        <img src={image} alt={name} className='person-img' />
-        <span className="quote-icon">
-          {setPlayerIcon(position)}
-        </span>
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">{setPlayerIcon(position)}</span>
       </div>
+      <h4 className="name">
+        {squad_number} {name}
+        {nationality}
+      </h4>
+      <h4 className="position">{position}</h4>
+      <p className="career">{career}</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">random</button>
     </article>
-  )
+  );
 };
 
 export default Main;
